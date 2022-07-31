af62a789459156d84594d5cf73c7bab2e19d131e
unknown <aboodhanafi10@gmail.com>
2022-07-31 09:06:14 +0300

    last-review-firstSection

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Container } from '../../GlobelStyle';
-import BasicButtons from '../Buttons';
+import { Container } from "../../GlobelStyle";
+import BasicButtons from "../Buttons";
 import NavBar from "../NavBar";
 import { H1, HeroContent, HeroSection, Par } from "./style";
-const Hero: React.FC = ()=> {
-
+const Hero: React.FC = () => {
   return (
     <HeroSection>
       <Container>
         <NavBar />
-        <HeroContent >
-    <H1>
-      <span>NEW</span> COLLECTION 
-      </H1>
+        <HeroContent>
+          <H1>
+            <span>NEW</span> COLLECTION
+          </H1>
 
-      <Par>Our Easiest Chuck-On-And-Go Staples Come With A Serious Style Heritage That’s Liberating, Sexy, Comfy And Supremely Cool. </Par>
-     <BasicButtons/>
-    </HeroContent>
+          <Par>
+            Our Easiest Chuck-On-And-Go Staples Come With A Serious Style
+            Heritage That’s Liberating, Sexy, Comfy And Supremely Cool.{" "}
+          </Par>
+          <BasicButtons />
+        </HeroContent>
       </Container>
-   
     </HeroSection>
   );
-}
+};
 
 export default Hero;
