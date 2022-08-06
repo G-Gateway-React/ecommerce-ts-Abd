a04af31cfb50ae6682e9cacfe8389d93c74df749
unknown <aboodhanafi10@gmail.com>
2022-08-06 19:24:28 +0300

    authentication done

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,41 +1,35 @@
 import React from "react";
 import { LeftNavbarData, RightNavbarData } from "../../Assets";
 import { Item, LeftSection, Logo, NavWrapper, RightSection } from "./style";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-
-const NavBar: React.FC = () => {
-    return(
-       
-            <NavWrapper>
-            < LeftSection>
-            {LeftNavbarData.map((item)=>{ 
-                return(
-                    <Item>{item.text}</Item>
-                ) 
-
-            })}
-           </LeftSection>
-           <Logo />
-              <RightSection>
-
-           {RightNavbarData.map((item)=>{
-            return(
-                
-              <Item>{ item.text ? item.text : <FavoriteBorderIcon/>}</Item>
-           
-            )
-           })}
-
-              </RightSection>
-               
-      
-
-           </NavWrapper>
-       
-    )
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+interface NavbarProps {
+  style?: React.CSSProperties;
 }
 
+const NavBar: React.FC<NavbarProps> = (props) => {
+  return (
+    <NavWrapper>
+      <LeftSection>
+        {LeftNavbarData.map((item) => {
+          return (
+            <Item key={item.id} style={props.style}>
+              {item.text}
+            </Item>
+          );
+        })}
+      </LeftSection>
+      <Logo style={{ backgroundColor: "black" }} />
+      <RightSection>
+        {RightNavbarData.map((item) => {
+          return (
+            <Item key={item.id} style={props.style}>
+              {item.text ? item.text : <FavoriteBorderIcon />}
+            </Item>
+          );
+        })}
+      </RightSection>
+    </NavWrapper>
+  );
+};
+
 export default NavBar;
-
-
-
